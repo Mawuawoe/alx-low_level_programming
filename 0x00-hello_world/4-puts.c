186c9fd67f1a3ff91092c527db4cbd1cd8c8b442
Mawuawoe <desmonddzakago@gmail.com>
2022-09-09 20:09:29 +0000

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints the strings in the func.
+ * Discription: applying the main func.
  * this C program print 'Programming is like building a multilingual puzzle'
  * return:0 ie ending the program
  */
 
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
