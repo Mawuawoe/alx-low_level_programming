b21cb3ead084893dbd557a590fefdf662fbb08f8
Mawuawoe <desmonddzakago@gmail.com>
2022-09-16 15:55:17 +0000

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if positive or negative
+ * Description: using the main.h file
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
