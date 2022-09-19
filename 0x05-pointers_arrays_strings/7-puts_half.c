44fc9328b1557e2000fdbaba4567994c33d3d616
Mawuawoe <desmonddzakago@gmail.com>
2022-09-19 21:12:50 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 }
 
 /**
- * _strlen - returns the length of a string 
+ * _strlen - returns the length of a string
  * @s: string
  *
  * Return: the length of the given string
