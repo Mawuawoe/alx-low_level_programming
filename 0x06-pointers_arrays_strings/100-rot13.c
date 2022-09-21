8caa7a5052edb44769a1803211d0c73df5094601
Mawuawoe <desmonddzakago@gmail.com>
2022-09-21 17:34:56 +0000

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,22 +12,15 @@ char *rot13(char *str)
 
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-				'T', 'U', 'V', 'W', 'X', 'Y',
-				'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'n', 'o', 'p', 'q',
-				'r', 's', 't', 'u', 'v', 'w',
-				'x', 'y', 'z', 'a', 'b', 'c',
-				'd', 'e', 'f', 'g', 'h', 'i',
-				'j', 'k', 'l', 'm'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
 	while (str[++index1])
 	{
-		for (index2 = 0; index2 < 52; index++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
 			if (str[index1] == alphabet[index2])
 			{
-				str[index] = rot13key[index];
+				str[index] = rot13key[index2];
 				break;
 			}
 		}
