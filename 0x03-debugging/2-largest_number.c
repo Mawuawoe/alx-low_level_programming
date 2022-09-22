cf543bdc0f9d9c617661448c383300f344a385e4
Mawuawoe <desmonddzakago@gmail.com>
2022-09-22 14:32:25 +0000

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,6 +7,7 @@
  * @c: third int
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -15,14 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
 	}
-
+	else
+	{
+		largest = b;
+	}
 	return (largest);
 }
