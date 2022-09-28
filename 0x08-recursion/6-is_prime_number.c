34b1c1bbcd4130fb7614f549d0aa2b3c7d5e4f39
Mawuawoe <desmonddzakago@gmail.com>
2022-09-28 12:46:09 +0000

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,7 +16,7 @@ int is_prime_number(int n)
 }
 
 /**
- *is_prime_number - return the 1 if n is prime
+ *is_prime - return the 1 if n is prime
  * @n: number to be cheacked
  * @start: number to start checking from
  * Return: 1 if n is prime, 0 otherwise
