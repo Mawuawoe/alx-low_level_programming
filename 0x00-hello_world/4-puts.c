3a8f124ce08457c00351de8aaec5d44a1afc1790
Mawuawoe <desmonddzakago@gmail.com>
2023-12-16 19:53:24 +0000

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the strings in the put function
+ *
+ * Discription: using the main function
+ * this C program print 'Programming is like building a multilingual puzzle'
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
