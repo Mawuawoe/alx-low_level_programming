aca4fa579d45bef628600e04f9f0f28956474478
Mawuawoe <desmonddzakago@gmail.com>
2024-02-07 21:21:11 +0000

    all

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
     newNode = (listint_t *)malloc(sizeof(listint_t));
     /*malloc can fail so we check if newnode is null*/
     /*anytime malloc fails it returns null*/
+    if (head == NULL)
+    {
+        return(NULL);
+    }
     if (newNode == NULL)
     {
         return(NULL);
