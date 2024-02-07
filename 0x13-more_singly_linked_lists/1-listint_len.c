03999c5b71cc0a16e544abfe3f4b63f757009798
Mawuawoe <desmonddzakago@gmail.com>
2024-02-07 20:55:45 +0000

    all

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - function that return the number of element in list
+ * @h: head of the list
+ * Return: total number of elements/nodes in the list
+*/
+
+/*length of the list*/
+size_t listint_len(const listint_t *h)
+{
+    int i = 1;
+    const listint_t *temp;
+    temp = h;
+    if (temp == NULL)
+    {
+        return(0);
+    }
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+        i ++;
+    }
+    return(i);
+}
