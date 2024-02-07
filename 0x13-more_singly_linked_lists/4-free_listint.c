87d843f13f44e2c57be575e71acfbf00bdcfe728
Mawuawoe <desmonddzakago@gmail.com>
2024-02-07 22:14:11 +0000

    all

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a nodes in link list one by one
+ * @head: pointer to the first node of the list
+ * return: nothing
+*/
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+    
+    /*if(head == NULL) then link is an empty link list*/
+   /* {
+        return;
+    }*/
+    while((temp = head) != NULL)
+    {
+        head = head->next;
+        free(temp);
+    }
+}
