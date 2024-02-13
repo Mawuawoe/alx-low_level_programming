cf4260cad9e294786c33c340c63236172629324f
Mawuawoe <desmonddzakago@gmail.com>
2024-02-13 15:44:56 +0000

    task 5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
     unsigned int temp;
-    if(index > sizeof(unsigned int) * 8)
+    if(index > (sizeof(unsigned int) * 8) || n == NULL) 
     {
         return(-1);
     }
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * clear_bit - function that set the value of a bit at a given index to 0
+ * @n: the given number
+ * @index: the given inndex
+ * Return: 1 if it works and -1 if it fails
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    unsigned int mask;
+    if(index > (sizeof(unsigned int) * 8) || n == NULL) 
+    {
+        return(-1);
+    }
+    else
+    {
+        mask = 1;
+        mask = mask << index;
+        if((( (*n) >> index) & 1) == 1)
+        {
+            *n = *n ^ mask;
+        }
+        return(0);
+    }
+}
