0e4ee789eca4fba1596b5f8084bbf0e09483a31b
Mawuawoe <desmonddzakago@gmail.com>
2024-03-21 19:21:52 +0000

    task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - function to print all items in a double linked list
+ * @h: the head of the list
+ * Return: the number of items in the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *ptr;
+size_t num_Nodes = 0;
+ptr = h;
+while (ptr != NULL)
+{
+num_Nodes++;
+ptr = ptr->next;
+}
+return (num_Nodes);
+}
